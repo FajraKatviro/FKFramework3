93e8d6f4c107617ea070df50ea26ddcd8ebc26c3
Fajra Katviro <fajrakatviro@gmail.com>
2017-03-23 04:23:33 +0300

    local connector implemented

--- modules/FKNet/service/FKConnection.cpp
+++ modules/FKNet/service/FKConnection.cpp
@@ -2,10 +2,53 @@
 
 #include "FKLogger.h"
 
-FKConnection::FKConnection(QObject* parent) : QObject(parent){
+FKConnection::FKConnection(QObject* parent, qint32 id):QObject(parent),_id(id){
     FK_CONSTRUCTOR
 }
 
 FKConnection::~FKConnection(){
     FK_DESTRUCTOR
+}
+
+void FKConnection::sendMessage(const qint32 messageType, const FKConnection::MessageContainer& message){
+    emit messageSent(messageType,message.isonRepresentation());
+}
+
+qint32 FKConnection::getId() const{
+    return _id;
+}
+
+void FKConnection::recieveMessage(const qint32 messageType, const QJsonObject message){
+    emit messageRecieved(_id,messageType,message);
+}
+
+FKConnection::MessageContainer::MessageContainer(const QJsonObject& message):_jsonRepresentation(message){
+
+}
+
+FKConnection::MessageContainer::MessageContainer(const QByteArray& data):_jsonRepresentation(jsonFromData(data)),_byteRepresentation(data),_byteRepresentationPrepared(true){
+
+}
+
+const QJsonObject& FKConnection::MessageContainer::isonRepresentation() const{
+    return _jsonRepresentation;
+}
+
+const QByteArray& FKConnection::MessageContainer::byteRepresentation() const{
+    if(_byteRepresentationPrepared){
+        return _byteRepresentation;
+    }
+    _byteRepresentation = dataFromJson(_jsonRepresentation);
+    _byteRepresentationPrepared = true;
+    return _byteRepresentation;
+}
+
+QJsonObject FKConnection::MessageContainer::jsonFromData(const QByteArray& data){
+    todo;
+    return QJsonObject();
+}
+
+QByteArray FKConnection::MessageContainer::dataFromJson(const QJsonObject& message){
+    todo;
+    return QByteArray();
 }
--- modules/FKNet/service/FKConnection.h
+++ modules/FKNet/service/FKConnection.h
@@ -3,14 +3,44 @@
 
 #include <QObject>
 
+#include <QJsonObject>
+#include <QByteArray>
+
 #include "FKNet_global.h"
 
 class FK_NET_EXPORT FKConnection : public QObject{
     Q_OBJECT
 public:
-    explicit FKConnection(QObject* parent = 0);
+    explicit FKConnection(QObject* parent = 0, qint32 id = -1);
     ~FKConnection();
 
+    class MessageContainer{
+    public:
+        MessageContainer(const QJsonObject& message);
+        MessageContainer(const QByteArray& data);
+        const QJsonObject& isonRepresentation()const;
+        const QByteArray& byteRepresentation()const;
+    private:
+        static QJsonObject jsonFromData(const QByteArray& data);
+        static QByteArray dataFromJson(const QJsonObject& message);
+        const QJsonObject _jsonRepresentation;
+        mutable QByteArray _byteRepresentation;
+        mutable bool _byteRepresentationPrepared = false;
+    };
+
+    virtual void sendMessage(const qint32 messageType,const FKConnection::MessageContainer& message);
+
+    qint32 getId()const;
+
+protected slots:
+    void recieveMessage(const qint32 messageType,const QJsonObject message);
+
+signals:
+    void messageSent(const qint32 messageType,const QJsonObject message);
+    void messageRecieved(const qint32 connectionId, const qint32 messageType, const QJsonObject message);
+
+private:
+    qint32 _id;
 };
 
 #endif // FKCONNECTION_H
